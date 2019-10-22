5a64bce7229b772567faa58b1a8219613e6a9ad8
Cooper <cooper@pop-os.localdomain>
2019-10-22 00:09:16 -0700

    added auth, not finished yet

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,36 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
-import { User } from 'src/users/entities/user.entity';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/entities/user.entity';
 import * as argon2 from 'argon2';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  async validateUser(username: string, password: string): Promise<any>{
-    const userRecord = await this.usersService.findOne({username})
-    const matches = await argon2.verify(userRecord.password, password)
-    if (userRecord && userRecord.password)
+  async validateUser(email: string, password: string): Promise<any> {
+    const userRecord = await this.usersService.findOne({ email });
+    console.log(userRecord);
+    const matches = await argon2.verify(userRecord.password, password);
+    if (userRecord && matches) {
+      const { password, ...result } = userRecord;
+      return result;
+    }
+    return null;
+  }
+
+  async generateJWT(user: any) {
+    const payload = {
+      sub: user.id,
+      email: user.email,
+      username: user.username,
+    };
+
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
   }
 }
---
+++ src/gql-jwt.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { AuthGuard } from '@nestjs/passport';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
+import { AuthenticationError } from 'apollo-server-core';
+
+@Injectable()
+export class GqlJwtGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context);
+    const { req } = ctx.getContext();
+
+    return super.canActivate(new ExecutionContextHost([req]));
+  }
+
+  handleRequest(err: any, user: any) {
+    if (err || !user) {
+      throw err || new AuthenticationError('Authentication failed.');
+    }
+    return user;
+  }
+}
